6204da9e82b4fc65b0783c837a3fe442a783cf84
Owner <Owner@DESKTOP-2TPH2RF>
2024-05-22 01:10:33 +0530

    Add project files.

---
+++ CrudeUsingCollection/Employee1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudeUsingCollection
+{
+    internal class Employee1 :IComparable<Employee1>
+    {
+        public string name;
+        public int sallary;
+        public Employee1(string name ,int sallary)
+        {
+            this.name = name;
+            this.sallary = sallary;
+
+        }
+
+        public int CompareTo(Employee1 other)
+        {
+            if (this.sallary > other.sallary)
+            {
+                return 1;
+            }
+            else if (this.sallary< other.sallary)
+            { 
+                        
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+         
+        }
+        public override string ToString()
+        {
+            return $"name {name} ,sallary {sallary}";
+            
+        }
+    }
+   
+    
+}
---
+++ CrudeUsingCollection/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudeUsingCollection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* EmployeeCrude crude = new EmployeeCrude();
+
+             int option = 0;
+             do
+             {
+                 Console.WriteLine("1.Add Employee");
+                 Console.WriteLine("2.Update employee");
+                 Console.WriteLine("3.Delete employee");
+                 Console.WriteLine("4.Display all employee");
+                 int op = Convert.ToInt32(Console.ReadLine());
+                 switch (op)
+                 {
+                     case 1:
+
+                         Employee e1 = new Employee();
+                         Console.WriteLine("Enter employee id");
+                         e1.Id = Convert.ToInt32(Console.ReadLine());
+                         Console.WriteLine("Enter eplyee name");
+                         e1.Name = Console.ReadLine();
+                         Console.WriteLine("Enter employee sallary");
+                         e1.sallary = Convert.ToDouble(Console.ReadLine());
+                         crude.AddEmployee(e1);
+                         break;
+
+
+
+                     case 2:
+                         Employee e2 = new Employee();
+                         Console.WriteLine("Enter id to modify");
+                         e2.Id = Convert.ToInt32(Console.ReadLine());
+                         Console.WriteLine("Enter employye name");
+                         e2.Name = Console.ReadLine();
+                         Console.WriteLine(" Enter employye Sallary");
+                         e2.sallary = Convert.ToDouble(Console.ReadLine());
+                         crude.UpdateEmployee(e2);
+                         break;
+
+
+                     case 3:
+
+                         Console.WriteLine("Enter id delete");
+                         int id = Convert.ToInt32(Console.ReadLine());
+                         crude.DeleteEmolyee(id);
+                         break;
+
+                     case 4:
+                         List<Employee> list = crude.GetEmployees();
+                         foreach (Employee e in list)
+                         {
+                             Console.WriteLine($"{e.Id}  {e.Name} {e.sallary}");
+                         }
+                         break;
+
+
+                 }
+                 Console.WriteLine("Enter 1 option for continue");
+                 option = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+             }
+             while (option == 1);*/
+
+           /* Employee1 rahul = new Employee1("rahul", 45000);
+            Employee1 rakesh = new Employee1("rakesh", 50000);
+
+            int result = rahul.CompareTo(rakesh);
+            if(result == 1)
+            {
+                Console.WriteLine("Rahul has more sallary than rakesh");
+
+
+            }
+            else if(result == -1) 
+            {
+                Console.WriteLine("Rakesh has greater sallary than rahul");
+            }
+            else
+            {
+                Console.WriteLine("Both have same sallary");
+            }*/
+
+          
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
+    
+
